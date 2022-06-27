a7e7ab9211de9b8d0405dbc748915f3f38b8d6c0
LindaRoze <linda@isoft.lv>
2022-06-27 14:13:17 +0300

    [master]Project update

--- src/test/java/practice/BrowserTest.java
+++ src/test/java/practice/BrowserTest.java
@@ -1,5 +1,7 @@
+
 package practice;
 
+import lombok.extern.log4j.Log4j;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.testng.Assert;
@@ -8,19 +10,24 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Log4j
 public class BrowserTest {
+
+    private final String GOOGLE_URL = "https://www.google.lv/";
     ChromeDriver driver;
 
-    private final String GOOGLE_URL = "https://google.lv";
-@BeforeTest
-public void setProperties(){
-    System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-    Logger.getLogger("orc.openqa.selenium").setLevel(Level.OFF);
+    @BeforeTest
+    public void setProperties() {
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+        Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
+    }
 
-}
     @BeforeMethod(alwaysRun = true)
     public void openBrowser() {
+        log.info("Initializing ChromeDriver");
         driver = new ChromeDriver();
         driver.get(GOOGLE_URL);
     }
